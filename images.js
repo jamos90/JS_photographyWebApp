bf9a9d1d24b13d8202104455e47c19f0c4581bad
jamos90 <jamos_98@hotmail.com>
2018-08-05 20:15:07 +0100

    basic funcntions working

---
+++ images.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () =>{
+   let content = document.getElementById("image");
+    content.onmouseover = function () {
+      let mouseOver = document.
+
+    }
+    onMouse.textContent = "Click to add this image to your basket";
+});
