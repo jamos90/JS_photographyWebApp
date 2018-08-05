f653d877b5b465b9f196554f2fbe7b9b3f2ec864
jamos90 <jamos_98@hotmail.com>
2018-08-05 17:43:15 +0100

    slide show linking through to index page

--- slideshow.js
+++ slideshow.js
@@ -1,7 +1,25 @@
-function changeImage() {
-  const imageScoure = document.getElementById("image").getAttribute("src");
-  const currentImageNumber = imageScoure.substring(imageScoure.lastIndexOf("/") +1 ,imageScoure.lastIndexOf("/") +2);
+ document.addEventListener('DOMContentLoaded', () =>
+  var intervalId;
 
-  //takes the current image source and changes it to the current image number, one at the start, to it's value plus 1.
-  document.getElementById("src").setAttribute("src", "/slideshowimages" + (currentImageNumber ++) + ".jpg"  );
-}
+   function startSlideShow() {
+     intervalId = setInterval(changeImage, 3000);
+   }
+
+   //will clear interval id ie stop the slideshow
+   function stopSlideShow() {
+     clearInterval(intervalId);
+   }
+
+  function changeImage() {
+    let imageScoure = document.getElementById("image").getAttribute("src");
+    let currentImageNumber = imageScoure.substring(imageScoure.lastIndexOf("/") +1 ,imageScoure.lastIndexOf("/") +2);
+
+    if (currentImageNumber === 12){
+      currentImageNumber = 0;
+    };
+    //number function changes currentImageNumber to a number and then adds one to it.
+
+    //takes the current image source and changes it to the current image number, one at the start, to it's value plus 1.
+    document.getElementById("image").setAttribute("src", "../public/slideshowimages/" + (Number(currentImageNumber) +1) + ".jpg");
+  };
+);
